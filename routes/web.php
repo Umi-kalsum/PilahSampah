ee16ddaa1e9a61b2e1d675f3e6e5b2938cd4d4d9
Umi-kalsum@ <ukalsumm3@gmail.com>
2024-12-04 14:26:05 +0700

    buat pembagian routes untuk dasboard dan landing page

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::domain('admin.'.env('APP_URL'))->group(function(){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('portal.welcome');
 });
 
 Route::get('/dashboard', function () {
